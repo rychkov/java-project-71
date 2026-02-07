558018f47cb8c93d269adef3a504329dcc154c64
Iurii Rychkov <yuri.rychkov@gmail.com>
2026-02-07 11:47:12 +0100

    Step 9.

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -8,14 +8,20 @@ import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class AppTest {
+
   private final PrintStream standardOut = System.out;
   private final ByteArrayOutputStream output = new ByteArrayOutputStream();
   private static final String FIXTURES_PATH = "src/test/resources/fixtures/";
+  private static final String COMPLEX = "complex/";
 
   @BeforeEach
   void setUp() {
@@ -27,24 +33,39 @@ class AppTest {
     System.setOut(standardOut);
   }
 
-  @Test
-  void testDiffJson() throws IOException {
-    String fileName1 = FIXTURES_PATH + "file1.json";
-    String fileName2 = FIXTURES_PATH + "file2.json";
-    String result = readFixture("diff1_2.txt");
+  @ParameterizedTest
+  @MethodSource("filePairsProvider")
+  void testDiff(String file1, String file2, String diff) throws IOException {
+    String expected = readFixture(diff);
 
-    App.main(new String[]{fileName1, fileName2});
-    assertEquals(result, output.toString(StandardCharsets.UTF_8).trim());
+    App.main(new String[]{file1, file2});
+
+    assertEquals(expected, output.toString(StandardCharsets.UTF_8).trim());
   }
 
-  @Test
-  void testDiffYaml() throws IOException {
-    String fileName1 = FIXTURES_PATH + "file1.yaml";
-    String fileName2 = FIXTURES_PATH + "file2.yaml";
-    String result = readFixture("diff1_2.txt");
-
-    App.main(new String[]{fileName1, fileName2});
-    assertEquals(result, output.toString(StandardCharsets.UTF_8).trim());
+  static Stream<Arguments> filePairsProvider() {
+    return Stream.of(
+        Arguments.of(
+            FIXTURES_PATH + "file1.json",
+            FIXTURES_PATH + "file2.json",
+            "diff1_2.txt"
+        ),
+        Arguments.of(
+            FIXTURES_PATH + "file1.yaml",
+            FIXTURES_PATH + "file2.yaml",
+            "diff1_2.txt"
+        ),
+        Arguments.of(
+            FIXTURES_PATH + COMPLEX + "file1.json",
+            FIXTURES_PATH + COMPLEX + "file2.json",
+            COMPLEX + "diff1_2.txt"
+        ),
+        Arguments.of(
+            FIXTURES_PATH + COMPLEX + "file1.yaml",
+            FIXTURES_PATH + COMPLEX + "file2.yaml",
+            COMPLEX + "diff1_2.txt"
+        )
+    );
   }
 
   private String readFixture(String fileName) throws IOException {
